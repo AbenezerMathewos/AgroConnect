b07b13a9586ce1437c164ead0a0744d1174c201f
Abenezer <mathewosa9@gmail.com>
2026-08-14 22:07:41 -0600

    feat: create advisory service to handle API requests for diagnostic reports

--- frontend/src/services/advisoryService.js
+++ frontend/src/services/advisoryService.js
@@ -3,4 +3,5 @@ import api from './api';
 export const advisoryService = {
   getAll: (params) => api.get('/advisory', { params }).then((res) => res.data),
   getById: (id) => api.get(`/advisory/${id}`).then((res) => res.data),
+  diagnose: (payload) => api.post('/advisory/diagnose', payload).then((res) => res.data),
 };
